24c3bb95356a408ae29a9845d8fec77541fc0725
SirJerryJones <jerryjones008@gmail.com>
2022-05-27 23:09:26 +0530

    Final commit for v1.0

--- index.php
+++ index.php
@@ -1,84 +1,35 @@
 <?php
 /**
-* Write a production-ready function in PHP that sums the numbers in a file and outputs details of the results.
+*   Write a production-ready function in PHP that sums the numbers in a file and outputs details of the results.
 *
-* @author     Jerry Jones <jerryjones@live.in>
+*   @author     Jerry Jones <jerryjones@live.in>
 *
-* @param    string  $pathtofile
-* @return   integer $output
-* @access   public
+*   @param    string  $pathtofile
+*   @return   array $output
+*   @access   public
 */
 // Enabling strict mode for data type
 declare(strict_types=1);
-/* Enabling error reporting to show all error for development.
-On production error reporting should be off and enable logging the error
+/*  Enabling error reporting to show all error for development.
+    On production error reporting should be off and enable logging the error
 */
 error_reporting(E_ALL);
-//global $processed;
 
-function sum(string $pathToFile)//: string
+function sumFiles(string $pathToFile, $results = array()): array
 {
-    $processed = array();
+    $contents = file($pathToFile, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    // Calculate the sum and store it in array for final output
+    $results[$pathToFile] = array_sum($contents);
 
-    if(file_exists($pathToFile)){
-        //store processed files in an array to avoid repetation.
-        array_push($processed, strtolower($pathToFile));
-
-        //read the file for further processing
-        $contents = file($pathToFile, FILE_IGNORE_NEW_LINES);
-        $process = extractFileName($contents);
-        $fileGroup = array_unique($process['file']);
-
-        if(count($fileGroup) > 1){ //check if there are multiple files in a text file
-            $i = 1;
-            foreach($fileGroup as $file){
-
-                if(in_array(strtolower($file), $processed)){ //skip if already processed
-                    echo 'i was in<br>';
-                    continue;
-                }
-
-                array_push($processed, strtolower($file."-{$i}")); //add processed files into array
-                // $output = 'ok';
-                $i++;
-                echo $file;
-                echo ' --i was out<br>';
-            }
-            // $output = $fileGroup;
-        }else{
-            if(!in_array(strtolower($fileGroup[0]), $processed)){ //check if processed
-                array_push($processed, strtolower($fileGroup[0])); //add processed files into array
-            }
-            
-            // $output = count($fileGroup);
-        }
-    }else{
-        die("ERROR: Unable to open file!");
-    }
-
-    // return $output;
-    //return $processed;
-}
-
-function extractFileName(array $contents) {
-    $output = array();
-    $fileName = array();
-
-    foreach($contents as $value)
-    {
-        if(!is_numeric($value)){
-            $path_parts = pathinfo($value);
-            if($path_parts['extension'] == 'txt'){
-                array_push($fileName, $value);
-            }
+    // Loop through the contents of the file, get file name if presente
+    for ($i=0; $i < count($contents); $i++) {
+        if (!is_numeric($contents[$i]) && !array_key_exists($contents[$i], $results)) {
+            $results = sumfiles($contents[$i], $results);
         }
     }
-    $output['file'] = $fileName;
-    $output['sum'] = array_sum($contents);
-    return $output;
+    return $results;
 }
 
-$output = sum('./uploads/A.txt');
+$output = sumFIles('./uploads/a.txt');
 echo"<pre>";
 print_r($output);
-// print_r(extractFile());
